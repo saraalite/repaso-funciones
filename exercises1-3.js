7df45e720141068628d5fc9f8d2f0f9f13656762
saraalite <saraalite@gmail.com>
2020-06-30 18:27:41 +0200

    Add fase/4

--- exercises1-3.js
+++ exercises1-3.js
@@ -1,5 +1,5 @@
 'use strict'
-//exercise 1
+//FASE 1
 
 /* const patata="patata"
 function ten(timesToRepeat,word){
@@ -11,7 +11,7 @@ function ten(timesToRepeat,word){
 ten( ) */
 
 
-//exercise 2
+// FASE 2
 
 /* function ten(word){
   for (let i = 0; i < 10; i = i + 1) {
@@ -24,7 +24,7 @@ ten("avocado")
 ten("hummus")
  */
 
-//exercise 3
+//FASE 3
 
 /* function ten(timesToRepeat,word){
     for (let i = 0; i < timesToRepeat; i = i + 1) {
@@ -35,3 +35,19 @@ const writeThis = ten;
 writeThis(5,"pizza")
 writeThis(18, "avocado")
 writeThis(3,"hummus") */ 
+
+
+//FASE 4
+/* function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const random = getRandomNumber(10);
+
+
+ function ten(random, word){
+    for (let i = 0; i < random; i = i + 1) {
+        console.log(word);
+      }
+}
+const writeThis = ten;
+writeThis(random, "caramelo") */
