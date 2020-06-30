fee70847dda66d2816a96f8ba7fb845099c41c31
saraalite <saraalite@gmail.com>
2020-06-30 18:11:56 +0200

    Add fase/3

---
+++ exercises1-3.js
@@ -0,0 +1,37 @@
+'use strict'
+//exercise 1
+
+/* const patata="patata"
+function ten(timesToRepeat,word){
+  for (let i = 0; i < 10; i = i + 1) {
+      console.log(patata);
+    }
+}
+
+ten( ) */
+
+
+//exercise 2
+
+/* function ten(word){
+  for (let i = 0; i < 10; i = i + 1) {
+      console.log(word);
+    }
+}
+
+ten("pizza") 
+ten("avocado")
+ten("hummus")
+ */
+
+//exercise 3
+
+/* function ten(timesToRepeat,word){
+    for (let i = 0; i < timesToRepeat; i = i + 1) {
+        console.log(word);
+      }
+}
+const writeThis = ten;
+writeThis(5,"pizza")
+writeThis(18, "avocado")
+writeThis(3,"hummus") */ 
